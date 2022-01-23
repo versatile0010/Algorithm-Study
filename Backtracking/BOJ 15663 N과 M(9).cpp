16e116d2b9f0ee85424dd6e1beb0f1577ba0b5b1
versatile0010 <wogusn@gmail.com>
2022-01-23 12:22:51 +0900

    Create BOJ 15663 N과 M(9).cpp
    BOJ 15663 N과 M(9) : 백트래킹

---
+++ Backtracking/BOJ 15663 N과 M(9).cpp
@@ -0,0 +1,53 @@
+/*
+BOJ : https://www.acmicpc.net/problem/15663
+backtracking N°ú M(9)
+Versatile0010
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+int input_arr[10];
+bool isused[10];
+
+void solve(int k)
+{
+	if (k == m)
+	{
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	else
+	{
+		int temp = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (!isused[i]&&temp != input_arr[i])
+			{
+				isused[i] = true;
+				arr[k] = input_arr[i];
+				temp = arr[k];
+				solve(k + 1);
+				isused[i] = false;
+			}
+		}
+
+
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> input_arr[i];
+	sort(input_arr, input_arr + n);
+	solve(0);
+
+	return 0;
+}
