34ecb956d541d00167f147fc9977d1f5932acdec
versatile0010 <wogusn@gmail.com>
2022-02-23 12:15:37 +0900

    Create BOJ 1463 1로 만들기.cpp

---
+++ DP/BOJ 1463 1로 만들기.cpp
@@ -0,0 +1,26 @@
+/*
+BOJ : https://www.acmicpc.net/problem/1463
+backtracking 1로 만들기
+Versatile0010
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int dp[1000005];
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	freopen("input.txt", "r", stdin);
+	int n;
+	cin >> n;
+	dp[1] = 0;
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0)dp[i] = min(dp[i / 2]+1,dp[i]);
+		if (i % 3 == 0)dp[i] = min(dp[i / 3]+1,dp[i]);
+	}
+	cout << dp[n] << '\n';
+	return 0;
+}
